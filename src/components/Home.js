c8e59e1b9bb40d2c947f625283c13356e8a22b8f
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-01 23:48:18 -0700

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home ME</h1>;
+  return (
+    <main>
+      <section className="flex mx-auto">
+        <div className="flex home">
+          <div className="blockquote text-center">
+            <p>
+              Hello, I'm{" "}
+              <span className="highlight display-5">Zahra Seyedi</span>.<br />
+              Full Stack Web Developer. <br />
+              Feel free to take a look at my recent project on my work page.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
