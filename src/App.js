8c14427a48d6ce39e1fe78858867abaf87d69705
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-01 22:09:21 -0700

    adding Navbar route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Project from "./components/Project";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
-        <Route component={Home} path="/" />
+        <Route component={Home} path="/" exact />
         <Route component={About} path="/about" />
         <Route component={Contact} path="/contact" />
         <Route component={Project} path="/project" />
+        <Route component={Resume} path="/resume" />
       </Switch>
     </BrowserRouter>
   );
