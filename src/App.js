ac4b458180064f20eb43e499c4236164accb5187
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-01 21:42:39 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Project from "./components/Project";
 
 function App() {
-  return <h1>React Portfolio</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path="/" />
+        <Route component={About} path="/about" />
+        <Route component={Contact} path="/contact" />
+        <Route component={Project} path="/project" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
