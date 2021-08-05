0884b70351b9316534141ac608776f4373e78b3e
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-04 22:54:18 -0700

    resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,41 @@
 import React from "react";
+import resume from "../assets/data/Zahra Seyedi RESUME.pdf";
 
 const Resume = () => {
-  return <div></div>;
+  return (
+    <div className="flex bg-dark">
+      <h3>Experience and Skills</h3>
+      <div className="pb-5">
+        <p>
+          Click here to download my{" "}
+          <a
+            href={resume}
+            rel="noreferrer"
+            download="Resume"
+            target="_blank"
+            className="resume "
+          >
+            Resume
+          </a>
+        </p>
+      </div>
+
+      <div className="card-container">
+        <div className="card">
+          <div className="card-header">
+            <h3>Proficiencies</h3>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">JavaScript</li>
+            <li className="list-group-item">MongoDB</li>
+            <li className="list-group-item">Express</li>
+            <li className="list-group-item">React</li>
+            <li className="list-group-item">Node</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
