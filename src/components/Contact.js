1e36c3aab11677ff2a2079d4f920d05881398e81
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-04 21:41:15 -0700

    comntact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,62 @@
-import React from "react";
-
 const Contact = () => {
-  return <h1>Contact ME</h1>;
+  return (
+    <div className="flex bg-dark">
+      <h3 className="pb-5 text-white">Contact Me</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <div className="contactForm">
+              <form id="contact-form" noValidate>
+                <div className="row formRow">
+                  <div className="col-6">
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control formInput"
+                      placeholder="Name"
+                    ></input>
+                  </div>
+                  <div className="col-6">
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control formInput"
+                      placeholder="Email address"
+                    ></input>
+                  </div>
+                </div>
+
+                <div className="row formRow">
+                  <div className="col">
+                    <input
+                      type="text"
+                      name="subject"
+                      className="form-control formInput"
+                      placeholder="Subject"
+                    ></input>
+                  </div>
+                </div>
+
+                <div className="row formRow">
+                  <div className="col">
+                    <textarea
+                      rows={3}
+                      name="message"
+                      className="form-control formInput"
+                      placeholder="Message"
+                    ></textarea>
+                  </div>
+                </div>
+                <button className="submit-btn" type="submit">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
